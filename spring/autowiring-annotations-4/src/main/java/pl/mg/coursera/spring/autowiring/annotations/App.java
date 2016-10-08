e626760162d0deee3120938979d6ee8c305e6a13
mgzik <mgzik@ospartner.pl>
2016-10-08 17:47:52 +0200

    Spring autowiring annotations init

---
+++ spring/autowiring-annotations-4/src/main/java/pl/mg/coursera/spring/autowiring/annotations/App.java
@@ -0,0 +1,13 @@
+package pl.mg.coursera.spring.autowiring.annotations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
