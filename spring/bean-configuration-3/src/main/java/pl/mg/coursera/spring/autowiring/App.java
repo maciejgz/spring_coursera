bb466f261256c966672416bcffd6e473d9f2088c
mgzik <mgzik@ospartner.pl>
2016-10-08 12:49:54 +0200

    18. autowiring init project

---
+++ spring/bean-configuration-3/src/main/java/pl/mg/coursera/spring/autowiring/App.java
@@ -0,0 +1,13 @@
+package pl.mg.coursera.spring.autowiring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
