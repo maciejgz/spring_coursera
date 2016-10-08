69166443a9a5052a51c4c9473e925da3a34afe04
mgzik <mgzik@ospartner.pl>
2016-10-08 17:02:25 +0200

    22. autowiring by constructor

--- spring/bean-configuration-3/src/main/java/pl/mg/coursera/spring/autowiring/Logger.java
+++ spring/bean-configuration-3/src/main/java/pl/mg/coursera/spring/autowiring/Logger.java
@@ -3,6 +3,12 @@ package pl.mg.coursera.spring.autowiring;
 public class Logger {
 
     private LogWriter consoleWriter;
+
+    public Logger(LogWriter consoleWriter, LogWriter fileWriter) {
+        this.consoleWriter = consoleWriter;
+        this.fileWriter = fileWriter;
+    }
+
     private LogWriter fileWriter;
 
     public LogWriter getConsoleWriter() {
