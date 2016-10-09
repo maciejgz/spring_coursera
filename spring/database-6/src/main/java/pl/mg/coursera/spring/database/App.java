b7f5bc883004a345ac9c3cc7f21c7eca08aafab1
mgzik <mgzik@ospartner.pl>
2016-10-09 18:48:27 +0200

    37. Database init

---
+++ spring/database-6/src/main/java/pl/mg/coursera/spring/database/App.java
@@ -0,0 +1,13 @@
+package pl.mg.coursera.spring.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
